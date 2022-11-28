9f0d03a55194b7dfb5fee5c2bdd8a2d00c03d0e9
Igor Carlos <igor.lima58@gmail.com>
2022-11-28 10:45:29 -0300

    First commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "../../App";
+
+class Button extends React.Component {
+  render() {
+    return <button>Botão</button>;
+  }
+}
+
+export default Button;
